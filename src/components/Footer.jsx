f4f663c6829c092ee61fa44a34980bc02bc4c006
hablu <131282586+hablu@users.noreply.github.com>
2024-03-03 19:42:03 +0600

    footer category create

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,15 +3,13 @@
 const Footer = () => {
   return (
     <div>
-      <footer
-        className="footer p-10 bg-[#212731] text-neutral-content"
-      >
+      <footer className="footer p-10 bg-[#212731] text-[#B6BDC3]">
         <nav>
-          <h6 className="footer-title">Services</h6>
-          <a className="link link-hover">Branding</a>
-          <a className="link link-hover">Design</a>
-          <a className="link link-hover">Marketing</a>
-          <a className="link link-hover">Advertisement</a>
+          <h6 className=" uppercase mb-4 text-[16px]">Categories</h6>
+          <a className="link text-[13px] mb-3 link-hover ">Web Builder</a>
+          <a className="link text-[13px] link-hover mb-3">Hosting</a>
+          <a className="link mb-3 text-[13px] link-hover">Data Center</a>
+          <a className="link mb-3 text-[13px] link-hover">Robotic-Automation</a>
         </nav>
         <nav>
           <h6 className="footer-title">Company</h6>
