16d4a33b6208a061f149a9b4771bb945685d3a1d
hablu <131282586+hablu@users.noreply.github.com>
2024-03-03 19:25:17 +0600

    error page create

---
+++ src/app/error.jsx
@@ -0,0 +1,15 @@
+"use client"
+
+const error = () => {
+  return (
+    <div>
+      <h1>
+        <p className="text-3xl text-red-500">
+          Something went wrong
+        </p>
+      </h1>
+    </div>
+  );
+};
+
+export default error;
