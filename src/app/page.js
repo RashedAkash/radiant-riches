89a298fa543ededc8705dc246da78d9840aaf19e
hablu <131282586+hablu@users.noreply.github.com>
2024-03-04 07:46:24 +0600

    sign up and deals section complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Footer from '@/components/Footer';
+import SignUp from '@/components/SignUp';
 import React from 'react';
 
 const page = () => {
   return (
     <div>
-      
+      <SignUp />
     </div>
   );
 };
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -2,8 +2,30 @@
 
 const SignUp = () => {
   return (
-    <div>
-      
+    <div className=" md:flex justify-around py-10 bg-[#FBFCFD]">
+      <div>
+        <h1 className=" text-[#5C6874] text-[32px]">
+          Sign up and get exclusive <br /> special deals
+        </h1>
+      </div>
+      <div>
+        <div class="w-full max-w-sm mx-auto mt-6 bg-transparent border rounded-md dark:border-none focus-within:border-blue-400 focus-within:ring focus-within:ring-blue-300 dark:focus-within:border-blue-300 focus-within:ring-opacity-40">
+          <form class="flex flex-col md:flex-row">
+            <input
+              type="email"
+              placeholder=""
+              class="flex-1 h-[52px] w-[445px] px-4 py-2 m-1 text-gray-700 placeholder-white rounded border-none appearance-none dark:text-gray-200 focus:outline-none focus:placeholder-transparent focus:ring-0"
+            />
+
+            <button
+              type="button"
+              class="h-[52px] w-[97px] rounded-l-none rounded-r-xl px-4 py-2 m-1 text-white transition-colors text-[14px] duration-300 transform bg-[#1B88F4] rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-400"
+            >
+             Sign up
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
