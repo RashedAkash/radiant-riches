a4bbf9735291f353872d1e9edb300dda38627a86
hablu <131282586+hablu@users.noreply.github.com>
2024-03-04 14:36:45 +0600

    web builder title added

--- src/components/WebBuilder.jsx
+++ src/components/WebBuilder.jsx
@@ -3,18 +3,24 @@ import WebBuilderCard from "./WebbuilderCard";
 
 
 const WebBuilder = () => {
-  return <div className=" bg-[#FBFCFD] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-    <div>
-      <WebBuilderOne  />
+  return (
+    <div className=" ">
+      <div>
+        <h1 className=" text-[#2C384A] text-[32px] mb-8 ">Related deals you might like for</h1>
+      </div>
+      <div className=" bg-[#FBFCFD] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div>
+          <WebBuilderOne />
+        </div>
+        <div>
+          <WebBuilderCard />
+        </div>
+        <div>
+          <WebBuilderCard />
+        </div>
+      </div>
     </div>
-    <div>
-      <WebBuilderCard />
-    </div>
-    <div>
-      <WebBuilderCard />
-    </div>
-
-  </div>;
+  );
 };
 
 export default WebBuilder;
