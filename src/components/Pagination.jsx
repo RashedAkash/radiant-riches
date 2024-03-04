0b512784859ff3d4a3b6143f1a973ca9aecbe715
hablu <131282586+hablu@users.noreply.github.com>
2024-03-04 09:24:06 +0600

    pagination responsive complete

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -2,7 +2,7 @@
 import { FaChevronRight } from "react-icons/fa";
 const Pagination = () => {
   return (
-    <section className=" mt-14 mb-12">
+    <section className=" mt-14 mb-12 overflow-x-auto">
       <p className=" flex items-center text-[#727D87] text-[14px] ">
         Home
         <span className=" pl-5 pr-4">
