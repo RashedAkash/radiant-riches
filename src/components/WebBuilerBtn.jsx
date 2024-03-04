a3ea3d93e61e2a589dc154c61f01ec8ebd3fc966
hablu <131282586+hablu@users.noreply.github.com>
2024-03-04 11:44:10 +0600

    web buildre button design complete

--- src/components/WebBuilerBtn.jsx
+++ src/components/WebBuilerBtn.jsx
@@ -3,7 +3,9 @@
 const WebBuilerBtn = () => {
   return (
     <div>
-      
+      <button className="btn btn-wide text-white bg-[#1B88F4] font-normal mt-5 text-[16px]">
+        View Deal
+      </button>
     </div>
   );
 };
--- src/components/WebbuilderCard.jsx
+++ src/components/WebbuilderCard.jsx
@@ -4,32 +4,42 @@ import WebBuilerBtn from "./WebBuilerBtn";
 
 const WebBuilderCard = () => {
   return (
-    <div className=" px-[30px] pb-[30px] pt-16 bg-white">
+    <div className=" px-[30px] pb-[30px]  bg-white">
       <div>
-        <Image src="/com.png" alt="com" width={141} height={103} />
+        <figure className="px-10 pt-10 ">
+          <Image
+            src="/com.png"
+            alt="com"
+            width={141}
+            height={103}
+            className=" "
+          />
+        </figure>
       </div>
-      <div className="  flex gap-1">
+      <div className="  flex gap-2">
         <div>
-          <p className=" bg-[#F2F4F7] rounded-lg  text-[14px] text-[#074786] ">
+          <p className=" py-2 pl-3 pr-4 bg-[#F2F4F7] rounded-lg  text-[14px] text-[#074786] ">
             20% Off
           </p>
         </div>
         <div>
-          <p className=" bg-[#F2F4F7] rounded-lg  text-[14px] text-[#074786] ">
+          <p className=" py-2 pl-3 pr-[30px] bg-[#F2F4F7] rounded-lg  text-[14px] text-[#074786] ">
             Limited time
           </p>
         </div>
       </div>
       <div>
-        <h1 className="text-center">Webbuilder 1</h1>
-        <p>
+        <h1 className="text-center text-[16px] font-bold text-[#626E79] mt-4 mb-5">
+          Webbuilder 1
+        </h1>
+        <p className="text-[#626E79] mb-4 text-[16px] ">
           Computer Modern clasic with wix <br /> support
         </p>
       </div>
-      <div className="flex gap-1">
-        <h1>$39.96</h1>
-        <p>$49.96</p>
-        <p>(20% Off)</p>
+      <div className="flex gap-4 items-end">
+        <h1 className=" text-[#5C6874] text-xl">$39.96</h1>
+        <p className=" text-[#9FA9B3] text-[14px]">$49.96</p>
+        <p className=" text-[#EF4C5D] text-[13px]">(20% Off)</p>
       </div>
       <div>
         <WebBuilerBtn />
