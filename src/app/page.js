88b61758f7fb8c6b9482deb8046d26cc2dceca35
hablu <131282586+hablu@users.noreply.github.com>
2024-03-04 08:04:04 +0600

    main title complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Footer from '@/components/Footer';
+import MainTitle from '@/components/MainTitle';
 import SignUp from '@/components/SignUp';
 import React from 'react';
 
 const page = () => {
   return (
     <div>
+      <MainTitle />
       <SignUp />
     </div>
   );
--- src/components/MainTitle.jsx
+++ src/components/MainTitle.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const MainTitle = () => {
   return (
     <div>
-      
+      <h1 className=" text-5xl text-[#2C384A]">
+        Best Website builders in the US
+      </h1>
     </div>
   );
 };
